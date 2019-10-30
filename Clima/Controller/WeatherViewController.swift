530e4e2bca3e69a286e414795fbc94bb9b861ffa
zuogang.li <bestmrlee@gmail.com>
2019-10-30 22:49:13 +0800

    Add textField delegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -17,15 +17,35 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
         // Do any additional setup after loading the view.
     }
     
     @IBAction func searchButtonPressed(_ sender: UIButton) {
-        
+        cityLabel.text = searchTextField.text
         print(searchTextField.text!)
+        searchTextField.endEditing(true)
     }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        cityLabel.text = searchTextField.text
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    private func textFieldDidEndEditing(_ textField: UITextField) -> Bool{
+        if searchTextField.text != ""{
+            return true
+        } else {
+            searchTextField.placeholder = "Type somothing..."
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 
 }
 
---
+++ Clima/Model/Clima.swift
@@ -0,0 +1,18 @@
+//
+//  Clima.swift
+//  Clima
+//
+//  Created by admin on 2019/10/30.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Clima {
+    
+    var position:(Double,Double)?
+    var condition = ""
+    var temp = 0
+    
+    
+}
